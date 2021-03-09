41abb236eb24cde98e12590fc72a6079a04408f3
executiveppl <75901152+executiveppl@users.noreply.github.com>
2021-03-09 14:34:34 +0100

    You6
    Try to fix Heroku

--- src/main/java/me/egidio/You69/Main.java
+++ src/main/java/me/egidio/You69/Main.java
@@ -1,6 +1,7 @@
 package me.egidio.You69;
 
 import me.egidio.You69.commands.PingPong;
+import me.egidio.You69.secret.Token;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.activity.ActivityType;
@@ -10,7 +11,7 @@ public class Main {
     public static final String Prefix = "?";
 
     public static void main(String[] args) {
-        DiscordApi Bot = new DiscordApiBuilder().setToken(System.getenv().get("TOKEN")).login().join();
+        DiscordApi Bot = new DiscordApiBuilder().setToken(Token.token).login().join();
         Bot.updateActivity(ActivityType.PLAYING, "??help");
 
         Bot.addListener(new PingPong());
---
+++ src/main/java/me/egidio/You69/secret/Token.java
@@ -0,0 +1,5 @@
+package me.egidio.You69.secret;
+
+public interface Token {
+    String token = "Nzk4MjIxNDUyMjAxMTY0ODUw.X_x3qA.qjZP--W0ezn7cmNAUmLPedfdc0s";
+}
