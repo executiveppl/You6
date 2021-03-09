8fced59220f09365b9f7e4c164afc0bc477bb235
executiveppl <75901152+executiveppl@users.noreply.github.com>
2021-03-09 14:58:36 +0100

    You6
    Modified activity from ??help to ?help

--- src/main/java/me/egidio/You69/Main.java
+++ src/main/java/me/egidio/You69/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         DiscordApi Bot = new DiscordApiBuilder().setToken(Token.token).login().join();
-        Bot.updateActivity(ActivityType.PLAYING, "??help");
+        Bot.updateActivity(ActivityType.PLAYING, "?help");
 
         Bot.addListener(new PingPong());
 
